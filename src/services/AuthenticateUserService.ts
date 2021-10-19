65574fd45c9031dfaccd6d6a82a50e8d3134fcc7
rwellingtonr <wellington-158@hotmail.com>
2021-10-19 00:20:11 -0300

    Set the Services

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,30 @@
+import axios from "axios"
+
+/*
+ * Receive the code (string)
+ * Recover the access_token on GitHub
+ * check whether the user already exist on DB,
+ *  TRUE = Generate the token
+ *  FALSE = Create on DB and generate a new token
+ * Return the token with the use's info
+ */
+
 class AuthenticateUserService {
-  async execute(code: string) {}
+  async execute(code: string) {
+    // Get the access token
+    const url = "https://github.com/login/oauth/access_token"
+    const response = await axios.post(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      },
+      headers: {
+        Accept: "application/json",
+      },
+    })
+    return response.data
+  }
 }
 
 export { AuthenticateUserService }
