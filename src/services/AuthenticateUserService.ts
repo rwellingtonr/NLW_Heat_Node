4605e7aa8a7b478c76be584ca61ef81709fb4c4a
rwellingtonr <wellington-158@hotmail.com>
2021-10-20 22:38:19 -0300

    added authorizations

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,26 +3,50 @@ import axios from "axios"
 /*
  * Receive the code (string)
  * Recover the access_token on GitHub
+ * Recover user's info on GitHub
  * check whether the user already exist on DB,
  *  TRUE = Generate the token
  *  FALSE = Create on DB and generate a new token
  * Return the token with the use's info
  */
 
+interface IAccessTokenResponse {
+  access_token: string
+}
+
+interface IUserResponse {
+  avatar_url: string
+  login: string
+  id: number
+  name: string
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     // Get the access token
-    const url = "https://github.com/login/oauth/access_token"
-    const response = await axios.post(url, null, {
-      params: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code,
-      },
+    const urlAccessToken = "https://github.com/login/oauth/access_token"
+    const urlGetUserData = "https://api.github.com/user"
+    // in the axiso token you might define which data it should've in the return
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(urlAccessToken, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      })
+    // Get the user's data on GitHub
+    // The header should allow the authorization token's type
+    // Between the <> you should pass the data's type you'd like to get
+    const response = await axios.get<IUserResponse>(urlGetUserData, {
       headers: {
-        Accept: "application/json",
+        authorization: `Bearer ${accessTokenResponse.access_token}`,
       },
     })
+
     return response.data
   }
 }
