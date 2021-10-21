d8e5cee07da3a7e1650047c88b2fca382b7d56be
rwellingtonr <wellington-158@hotmail.com>
2021-10-21 16:34:24 -0300

    created profile route

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express"
 import { AuthenticateUserController } from "./controller/AuthenticateUserController"
 import { CreateMessageController } from "./controller/CreateMessageControler"
 import { GetLast3MessagesController } from "./controller/GetLast3MessagesController"
+import { ProfileUserController } from "./controller/ProfileUserController"
 import ensureAuthenticated from "./middleware/ensureAuthenticated"
 
 const router = Router()
 
+/* POST METHOD */
 //It will work as a middleware
 router.post("/authenticate", new AuthenticateUserController().handle)
 
@@ -15,7 +17,9 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 )
-
+/* GET METHOD */
 router.get("/messages/last3", new GetLast3MessagesController().handle)
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
